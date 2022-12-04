a99cb71f910e3f8a678ffb8678219f49658784e5
linalstr5 <linalstr5@gmail.com>
2022-12-04 23:41:57 +0700

    commit DefaultFirebaseOptions

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ import 'app/routes/app_pages.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  )
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
